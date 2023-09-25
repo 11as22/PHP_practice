830e7ee03b007e4c20bb23e3b97105d229cc6d75
11as22 <ec2-user@ip-172-31-3-73.ap-northeast-1.compute.internal>
2023-09-25 07:31:05 +0000

    文字連携

---
+++ string_add.php
@@ -0,0 +1,10 @@
+<?php
+
+$string1 = "Hello";
+$string2 = "Wolrd";
+
+$string_dot = $string1 . $string2 . "\n";
+echo $string_dot;
+
+$string_dquote = "{$string1}{$string2} \n";
+echo $string_dquote;
