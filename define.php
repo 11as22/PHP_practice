5f13f7324b8677a759cdd4363311b2587c4bc8b0
11as22 <ec2-user@ip-172-31-3-73.ap-northeast-1.compute.internal>
2023-10-02 05:33:24 +0000

    確認

--- define.php
+++ define.php
@@ -6,3 +6,13 @@ define('DEF_STRING', "string");
 var_dump(DEF_INT);
 var_dump(DEF_FLOAT);
 var_dump(DEF_STRING);
+
+$num_int = 123;
+$num_string = "123";
+
+var_dump($num_int);
+var_dump($num_string);
+
+define('TEST_FLOAT', 1.23);
+
+var_dump(TEST_FLOAT);
