06931889aee2c69ba96fc4c02e2cd5a8bfa9e340
11as22 <ec2-user@ip-172-31-3-73.ap-northeast-1.compute.internal>
2023-10-02 04:39:47 +0000

    練習

---
+++ test.php
@@ -0,0 +1,11 @@
+<?php
+
+$price = 1800;
+$tax_price = $price * 1.1;
+
+echo "{$tax_price} \n";
+
+$first_name = "aaa";
+$second_name = "iii";
+
+echo "{$first_name}{$second_name} \n";
---
+++ type_bool.php
@@ -0,0 +1,12 @@
+<?php
+
+$bool_i = true;
+$bool_f = false;
+var_dump($bool_i);
+var_dump($bool_f);
+
+$bool_t_large = TRUE;
+var_dump($bool_t_large);
+
+$null_var = null;
+var_dump($null_var);
